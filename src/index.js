5648888e814d58933d4847401d51e6446a978413
Pradyumn24 <ayushpandit272204@gmail.com>
2023-09-13 08:44:54 +0530

    Project Skeleton setup done

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const { PORT } = require('./config/server-config');
+
+const app = express();
+
+const prepareAndStartServer = () => {
+
+    app.listen(PORT, () => {
+        console.log(`Server Started on Port: ${PORT}`);
+    });
+}   
+
+prepareAndStartServer();
