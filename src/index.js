6adc61593c520a5103e18b7b421901c5d5329b2d
Pradyumn24 <ayushpandit272204@gmail.com>
2023-09-19 00:55:00 +0530

    Completed signup with password encryption

--- src/index.js
+++ src/index.js
@@ -2,12 +2,32 @@ const express = require('express');
 
 const { PORT } = require('./config/server-config');
 
+const bodyParser = require('body-parser');
+
+const apiRoutes = require('./routes/index')
 const app = express();
+
+// const {User} = require('./models/index');
+// const bcrypt = require('bcrypt');
 
 const prepareAndStartServer = () => {
 
-    app.listen(PORT, () => {
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}));
+
+    app.use('/api', apiRoutes)
+
+
+
+    app.listen(PORT, async () => {
         console.log(`Server Started on Port: ${PORT}`);
+
+
+        // const incomingpassword = '24680890';
+        // const user = await User.findByPk(6);
+        // const response = bcrypt.compareSync(incomingpassword, user.password);
+        // console.log(response);
     });
 }   
 
