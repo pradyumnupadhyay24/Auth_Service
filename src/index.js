15e662d5555afd367c741deb048226e14350f1aa
Pradyumn24 <ayushpandit272204@gmail.com>
2023-09-21 10:23:39 +0530

    Added roles and user_roles through table

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ const express = require('express');
 
 const { PORT } = require('./config/server-config');
 
+const db = require('./models/index');
 const bodyParser = require('body-parser');
 
 const apiRoutes = require('./routes/index')
 const app = express();
 
-//const UserService = require('./services/user-service');
-
+const {User ,Role} = require('./models/index')
 // const UserRepository = require('./repository/user-repository');
 
 const prepareAndStartServer = () => {
@@ -23,8 +23,20 @@ const prepareAndStartServer = () => {
 
     app.listen(PORT, async () => {
         console.log(`Server Started on Port: ${PORT}`);
+        if(process.env.DB_SYNC) {
+            db.sequelize.sync({alter: true});
+        }
+        // for checking that the users and roles association
+        const u1 = await User.findByPk(5);
+        const r1 = await Role.findByPk(1);
+        // u1.addRole(r1);
+        const response=  await r1.hasUser(u1);
+        console.log(response);
 
 
+
+
+        
         // const incomingpassword = '24680890';
         // const user = await User.findByPk(6);
         // const response = bcrypt.compareSync(incomingpassword, user.password);
